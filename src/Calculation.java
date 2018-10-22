49364ab8b91a93f6986c03a845a9f9fa60e7da0f
Faworit <sergei_ryabzev@mail.ru>
2018-10-22 23:18:08 +0600

    flat

--- src/Calculation.java
+++ src/Calculation.java
@@ -36,21 +36,54 @@ public class Calculation {
 
     }
     public double squareOfFlat(ArrayList<Room> rooms){
+        double squareOfFlat=0;
+        double square;
+        ArrayList<Double> squareOfRoom = new ArrayList<>();
         ArrayList<ArrayList<Wall>> walls = new ArrayList<>();
         for(int i=0; i<rooms.size(); i++){
             Object obj = rooms.get(i);
             walls.add(((Room) obj).walls);
         }
         Object nsWall;
-        Object ewWall
+        Object ewWall;
         for(int i =0; i<walls.size(); i++){
 
             ArrayList<Wall> wal = new ArrayList<>();
             wal = walls.get(i);
             nsWall = wal.get(1);
             ewWall = wal.get(3);
+            square = ((Wall) nsWall).width*((Wall) ewWall).width;
+            squareOfRoom.add(square);
          // Написать формулу вычисления площади комнаты и добавить в коллекцию
         }
+        for(int i = 0; i<squareOfRoom.size(); i++){
+            squareOfFlat += squareOfRoom.get(i);
+        }
+        return squareOfFlat;
     }
 
+    public int countOfWall(ArrayList<Room> rooms){
+        int countOfWall=0;
+        ArrayList<Integer> idOfWall = new ArrayList<>();
+        ArrayList<ArrayList<Wall>> walls = new ArrayList<>();
+        for(int i=0; i<rooms.size(); i++){
+            Object obj = rooms.get(i);
+            walls.add(((Room) obj).walls);
+        }
+        Object nWall;
+       ArrayList<Object> singleWalls = new ArrayList<>();
+        for(int i =0; i<walls.size(); i++) {
+            ArrayList<Wall> wal = new ArrayList<>();
+            wal = walls.get(i);
+            for (int z = 0; z < wal.size(); z++) {
+                nWall = wal.get(i);
+                singleWalls.add(nWall);
+            }
+        }
+      for(int i=0; i<singleWalls.size(); i++){
+            nWall = singleWalls.get(i);
+            countOfWall = (Wall) nWall).id
+
+                  // разобраться в чем косяк и вытащить id по каждой стене
+      }
 }
